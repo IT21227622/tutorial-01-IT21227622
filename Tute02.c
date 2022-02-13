fed2ce612b2cd68bcb4ebbba015ce1690d81a967
Chamathka Samarasinghe <99588713+IT21227622@users.noreply.github.com>
2022-02-13 23:17:27 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -20,5 +20,23 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
   
+  int distance, extra;
+  float price;  //variable declaration
+  
+  printf("Input the distance van has travelled: ");
+  scanf("%d", &distance);
+  
+  if (distance <= 30)
+  {
+    price = distance * 50.00;
+  }
+  else
+  {
+    price = 1500.00 + (distance - 30) * 40.00 ;
+  }
+  
+  printf("The Final rent fee is:LKR %.2f", price);
+   
+  
   return 0;
 }
