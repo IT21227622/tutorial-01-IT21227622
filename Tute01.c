a162688b436e4491d3ba7c4f409b3457dec759d3
Chamathka Samarasinghe <99588713+IT21227622@users.noreply.github.com>
2022-02-13 22:08:35 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+   
+   int mark1, mark2; //Variable declaration
+   float avg;
+   
+   printf("Enter the marks of the first subject: ");
+   scanf("%d", &mark1);
+   printf("Enter the marks of the second subject: ");
+   scanf("%d", &mark2);
+   
+   avg = (mark1 + mark2) / 2;
+   
+   printf("Average of the two subjects: %f", avg);
   
   return 0;
 }
